b52e2554fcf6e140813d66cecb365a4fe4978584
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-06-01 10:56:53 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import StartPage from './components/StartPage';
 import NatalPage from './components/NatalPage';
+
 export const App = () => {
 
-
-
   return (
-    <h1>hello world</h1>
-  );
+<StartPage />
+    );
 }
+
 
 // this goes into the fronend folder
 /// npm i react-redux
