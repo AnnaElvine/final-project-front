8953cb444b04a9d08ac860383355e0e325828f9e
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-06-13 20:47:18 +0200

    ...

--- src/Pages/StartPage/index.js
+++ src/Pages/StartPage/index.js
@@ -17,6 +17,21 @@ const StartPage = () => {
 
 	const handleSubmit = (e) => { // this is a function that takes an event as an argument and returns nothing 
 		e.preventDefault();
+		fetch(`http://localhost:8080/zodiacSigns/${dateOfBirth}`, {
+			method: 'GET',
+			headers: {
+			  'Content-Type': 'application/json',
+			},
+		  })
+			.then((response) => response.json())
+			.then((result) => {
+			  // Handle the response from the backend
+			  console.log(result);
+			})
+			.catch((error) => {
+			  // Handle any errors that occur during the request
+			  console.error(error);
+			});
 
 		// Perform any desired actions with the form data here
 		console.log({
@@ -26,8 +41,10 @@ const StartPage = () => {
 			placeOfBirth*/
 		});
 	};
-console.log(dateOfBirth)
-console.log(name)
+
+
+/*console.log(dateOfBirth)
+console.log(name) */
 
 	return (
 		<>
@@ -70,12 +87,15 @@ console.log(name)
 						required /> */}
 
 					{/* <Link id="NatalBtn" to={`/natal`}>Submit</Link> */}
-					<Link id="NatalBtn" to={`/natal/${dateOfBirth}`}>Submit</Link>
+					{/*<Link id="NatalBtn" to={`/natal/${dateOfBirth}`}>Submit</Link> */}
+
+				<button type="submit">Submit</button>
 				</form>
 			</div> 
 		</>
 	);
 }
+
 
 export default StartPage;
 
